cc6257587cc67c964ced90ac41a885bdaaf2358b
SirEnri <Vlairrrrr@outlook.com>
2024-01-14 21:13:53 -0500

    add asserts into meshoperation

--- src/mesh/meshassert.cpp
+++ src/mesh/meshassert.cpp
@@ -1,12 +1,7 @@
 #include "meshassert.h"
 #include "meshcomponents.h"
-#include <boost/stacktrace.hpp>
 #include <sstream>
 
 void MeshAssert::_error(std::string& msg) {
-	std::stringstream ss;
-	ss << boost::stacktrace::stacktrace();
-	traceback = ss.str();
-	__debugbreak();
-	std::terminate();
+
 }
