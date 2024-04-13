3eb07b1d81bec2f15619621c4bd9ac434f2de758
Xinghua Han <Vlairrrrr@outlook.com>
2024-04-13 19:05:03 -0400

    feat(msc.cpp;-msc.h): impl morse smale comple & quasi-dual complex

--- src/msc/msc.cpp
+++ src/msc/msc.cpp
@@ -4,6 +4,7 @@
 #include "../mesh/components.h"
 #include "../mesh/display.h"
 #include <Eigen/Core>
+#include <iostream>
 using namespace quadro;
 MorseFunction::MorseFunction(Mesh* mesh) : MeshUserOperator(mesh) {
 	scalarFunction = mesh->createVertexAttribute<double>();
@@ -30,7 +31,7 @@ void MorseFunction::smoothScalarFunction(const Vertex* v) {
 void MorseFunction::extractWaveFunction() {
 	Eigen::MatrixXd LMatrix = laplacian(*mesh);
 	auto eigenresult = eigh(LMatrix);
-	Eigen::VectorXd eigenvector = eigenresult.second.col(mesh->getVertices().size()-20);
+	Eigen::VectorXd eigenvector = eigenresult.second.col(mesh->getVertices().size()-40);
 	for (auto& idVertex : mesh->getVertices()) {
 		const Vertex* vertex = &idVertex.second;
 		(*scalarFunction)[vertex->getMutable()] = eigenvector[idVertex.first];
@@ -94,42 +95,6 @@ void MorseFunction::extractMorseVertices() {
 		}
 		else {
 			setVertexType(v->getMutable(), REGULAR);
-		}
-	}
-	display->markVertexAttribute(vertexType->map<std::pair<bool, glm::vec4>>([](MorseFunction::VertexType vertexType)->std::pair<bool, glm::vec4> {
-		switch (vertexType)
-		{
-		case quadro::MorseFunction::NONE:
-			return { false, glm::vec4(0,0,0,0) };
-			break;
-		case quadro::MorseFunction::REGULAR:
-			return { false, glm::vec4(0,0,0,0) };
-			break;
-		case quadro::MorseFunction::MAXIMUM:
-			return { true, glm::vec4(1,0,0,1) };
-			break;
-		case quadro::MorseFunction::MINIMUM:
-			return { true, glm::vec4(0,0,1,1) };
-			break;
-		case quadro::MorseFunction::SADDLE:
-			return { true, glm::vec4(0,1,0,1) };
-			break;
-		default:
-			return { false, glm::vec4(0,0,0,0) };
-			break;
-		}
-	}).get());
-	for (auto& idHe : mesh->getHalfedges()) {
-		const Halfedge* he = &idHe.second;
-		double v = getScalarFunction(he->getTarget()) - getScalarFunction(he->getSource());
-		if (v > 0) {
-			display->setHalfedgeMark(he->getMutable(), glm::vec4(0,0,0,1), glm::vec4(1,1,1,1));
-		}
-		if (v < 0) {
-			display->setHalfedgeMark(he->getMutable(), glm::vec4(1,1,1,1), glm::vec4(0,0,0,1));
-		}
-		if (v == 0) {
-			display->setHalfedgeMark(he->getMutable(), glm::vec4(0,0,0, 1), glm::vec4(0, 0, 0, 1));
 		}
 	}
 }
@@ -202,8 +167,8 @@ MorseFunction::Traceline* MorseFunction::traceToMinimum(const Halfedge* he) {
 }
 
 MorseFunction::Traceline* MorseFunction::trace(const Halfedge* he, bool toMaximum) {
-	tracelines.push_back(Traceline());
-	Traceline* tr = &tracelines.back();
+	tracelines.push_back(std::make_unique<Traceline>());
+	Traceline* tr = tracelines.back().get();
 	tr->end1 = he->getSource();
 	(*vertexTracelines)[he->getSource()->getMutable()].push_back(tr);
 	while (toMaximum && getVertexType(he->getTarget()) != MAXIMUM || !toMaximum && getVertexType(he->getTarget()) != MINIMUM) {
@@ -212,12 +177,6 @@ MorseFunction::Traceline* MorseFunction::trace(const Halfedge* he, bool toMaximu
 		const Halfedge* tempHe = nullptr;
 		const Halfedge* heStart = he;
 		float tempVal = 0.0;
-		if (toMaximum) {
-			display->setHalfedgeMark(he->getMutable(), glm::vec4(1, 0, 0, 1), glm::vec4(1, 0, 0, 1));
-		}
-		else {
-			display->setHalfedgeMark(he->getMutable(), glm::vec4(0, 0, 1, 1), glm::vec4(0, 0, 1, 1));
-		}
 		do {
 			double vv1 = getGradientScalarFunction(he->getSym());
 			if (toMaximum && tempVal < vv1) {
@@ -238,13 +197,68 @@ MorseFunction::Traceline* MorseFunction::trace(const Halfedge* he, bool toMaximu
 	}
 	tr->line.push_back(he);
 	tr->end2 = he->getTarget();
+	tr->toMaximum = toMaximum;
 	(*vertexTracelines)[he->getTarget()->getMutable()].push_back(tr);
-	if (toMaximum) {
-		display->setHalfedgeMark(he->getMutable(), glm::vec4(0, 0, 0, 1), glm::vec4(1, 0, 0, 1));
+	return tr;
+}
+
+MorseFunction::Traceline* MorseFunction::trace(const Vertex* end1, const Vertex* end2) {
+	struct Tree {
+		const Vertex* v;
+		const Halfedge* he;
+		Tree* parent;
+	};
+	if (mesh->getHalfedge(end1, end2) != nullptr) {
+		tracelines.push_back(std::make_unique<Traceline>(Traceline{ end1, end2, {mesh->getHalfedge(end1, end2)}, getScalarFunction(end1) < getScalarFunction(end2) }));
+		return tracelines.back().get();
 	}
-	else {
-		display->setHalfedgeMark(he->getMutable(), glm::vec4(0, 0, 0, 1), glm::vec4(0, 0, 1, 1));
+	bool toMaximum = getScalarFunction(end1) < getScalarFunction(end2);
+	std::vector<std::unique_ptr<Tree>> trees;
+	trees.push_back(std::make_unique<Tree>(Tree({ end1, nullptr, nullptr})));
+	Tree* root = trees.back().get();
+	Tree* tree = root;
+	const Vertex* v = end1;
+	const Halfedge* he = v->getHalfedge();
+	std::vector<Tree*> currentRingTrees;
+	std::vector<Tree*> nextRingTrees;
+	std::unordered_map<int, int> traversed;
+	traversed[v->getId()] = 1;
+	bool find = false;
+	currentRingTrees.push_back(root);
+	while (true) {
+		for (Tree* t : currentRingTrees) {
+			v = t->v;
+			he = v->getHalfedge();
+			do {
+				if (v == end2) {
+					find = true;
+					tree = t;
+					break;
+				}
+				if (traversed[he->getSource()->getId()] == 1) {
+					continue;
+				}
+				trees.push_back(std::make_unique<Tree>(Tree{ he->getSource(), he, t }));
+				nextRingTrees.push_back(trees.back().get());
+				traversed[he->getSource()->getId()] = 1;
+			} while (he = he->getNext()->getSym(), he != v->getHalfedge());
+			if (find) {
+				break;
+			}
+		}
+		currentRingTrees = nextRingTrees;
+		if (find) {
+			break;
+		}
 	}
+	tracelines.push_back(std::make_unique<Traceline>(Traceline{ end1, end2, {}, getScalarFunction(end1) < getScalarFunction(end2) }));
+	Traceline* tr = tracelines.back().get();
+	while (tree != nullptr && tree->he!=nullptr) {
+		tr->line.push_front(tree->he);
+		tree = tree->parent;
+	}
+	(*vertexTracelines)[end1->getMutable()].push_back(tr);
+	(*vertexTracelines)[end2->getMutable()].push_back(tr);
 	return tr;
 }
 
@@ -253,6 +267,8 @@ void MorseFunction::operator()() {
 	extractMorseVertices();
 	extractMorseFunction();
 	cleanUpMorseFunction();
+	extractQuasiDualComplex();
+	paintMorseFunction();
 }
 
 double MorseFunction::getScalarFunction(const Vertex* v) {
@@ -266,6 +282,140 @@ void MorseFunction::setVertexType(Vertex* v, VertexType t) {
 	(*vertexType)[v] = t;
 }
 
-void MorseFunction::cleanUpMorseFunction() {
+void MorseFunction::removeMorseFunctionVertex(const Vertex* v) {
+	if ((*vertexTracelines)[v].size() == 0) {
+		return;
+	}
+	for (Traceline* tr : (*vertexTracelines)[v]) {
+		const Vertex* other = tr->end1 == v ? tr->end2 : tr->end1;
+		std::vector<Traceline*>& lines = (*vertexTracelines)[other->getMutable()];
+		lines.erase(std::remove(lines.begin(), lines.end(), tr), lines.end());
+		tr->isValid = false;
+		//tracelines.erase(std::remove_if(tracelines.begin(), tracelines.end(), [tr](const std::unique_ptr<Traceline>& tr1) {return tr1.get() != tr; }), tracelines.end());
+	}
+	(*vertexTracelines)[v->getMutable()].clear();
+}
 
+void MorseFunction::cleanUpMorseFunction() {
+	bool optimized = true;
+	do {
+		optimized = true;
+		for (const auto& idVertex : mesh->getVertices()) {
+			const Vertex* v = &idVertex.second;
+			if ((*vertexType)[v] != MAXIMUM && (*vertexType)[v] != MINIMUM) {
+				continue;
+			}
+			if ((*vertexTracelines)[v].size() != 2) {
+				continue;
+			}
+			Traceline* tr =
+				(*vertexTracelines)[v][0]->line.size() > (*vertexTracelines)[v][1]->line.size() ?
+				(*vertexTracelines)[v][1] : (*vertexTracelines)[v][0];
+			const Vertex* saddleV = tr->end1;
+			const Vertex* end1ToBeConnected = tr == (*vertexTracelines)[v][1]? (*vertexTracelines)[v][0]->end1 : (*vertexTracelines)[v][1]->end1;
+			const Vertex* end2ToBeConnected = nullptr;
+			for (Traceline* tr1 : (*vertexTracelines)[saddleV]) {
+				if ((*vertexType)[tr1->end2] == (*vertexType)[v] && tr1->end2 != v) {
+					end2ToBeConnected = tr1->end2;
+				}
+			}
+			removeMorseFunctionVertex(saddleV);
+			removeMorseFunctionVertex(v);
+			trace(end1ToBeConnected, end2ToBeConnected);
+			optimized = false;
+		}
+	} while (!optimized);
+}
+
+void MorseFunction::extractQuasiDualComplex() {
+	for (auto& idVertex : mesh->getVertices()) {
+		const Vertex* v = &idVertex.second;
+		if (getVertexType(v) != SADDLE) {
+			continue;
+		}
+		for (int i = 0; i < (*vertexTracelines)[v].size(); i++) {
+			Traceline* tr = (*vertexTracelines)[v][i];
+			if (getVertexType(tr->end2) != MINIMUM) {
+				continue;
+			}
+			Traceline* nextTr = (*vertexTracelines)[v][(i+1) % (*vertexTracelines)[v].size()];
+			trace(tr->end2, nextTr->end2);
+		}
+		removeMorseFunctionVertex(v);
+	}
+}
+
+
+void MorseFunction::paintMorseFunction() {
+	std::unordered_map<int, std::string> saddleIdMap;
+	std::unordered_map<int, std::string> extremeIdMap;
+	int saddleCount = 0;
+	int extremeCount = 0;
+	//for (const auto& uptr_tr : tracelines) {
+	//	const Traceline* tr = uptr_tr.get();
+	//	if (!tr->isValid) {
+	//		continue;
+	//	}
+	//	if (saddleIdMap.find(tr->end1->getId()) == saddleIdMap.end()) {
+	//		saddleIdMap[tr->end1->getId()] = "s" + std::to_string(++saddleCount);
+	//	}
+	//	if (extremeIdMap.find(tr->end2->getId()) == extremeIdMap.end()) {
+	//		extremeIdMap[tr->end2->getId()] = "v" + std::to_string(++extremeCount);
+	//	}
+	//	std::cout << tr->end1->getId() << " - " << tr->end2->getId() << std::endl;
+	//	//std::cout << tr.end1->getId() << " - " << tr.end2->getId() << std::endl;
+	//}
+
+	for (auto& idVertex : mesh->getVertices()) {
+		const Vertex* v = &idVertex.second;
+		if (getVertexType(v) == MAXIMUM || getVertexType(v) == MINIMUM) {
+			std::cout << "Extreme : " << (*vertexTracelines)[v].size() << std::endl;
+		}
+	}
+	
+	for (const auto& uptr_tr : tracelines) {
+		const Traceline& tr = *uptr_tr;
+		if (!tr.isValid) {
+			continue;
+		}
+		std::cout << tr.end1->getId() << " - " << tr.end2->getId() << std::endl;
+		glm::vec4 traceColor = glm::vec4(0, 0, 1, 1);
+		if (tr.toMaximum) {
+			traceColor = glm::vec4(1, 0, 0, 1);
+		}
+		display->setMarked(tr.end1->getMutable(), true);
+		display->setMarkColor(tr.end1->getMutable(), glm::vec4(0, 1, 0, 1));
+		display->setMarked(tr.end2->getMutable(), true);
+		display->setMarkColor(tr.end2->getMutable(), traceColor);
+		for (const Halfedge* he : tr.line) {
+			display->setHalfedgeMark(he->getMutable(), glm::vec4(1, 1, 1, 1), glm::vec4(traceColor));
+		}
+	}
+	//for (const auto& uptr_tr : tracelines) {
+	//	const Traceline& tr = *uptr_tr;
+	//	if (tr.isValid) {
+	//		continue;
+	//	}
+	//	for (const Halfedge* he : tr.line) {
+	//		display->setHalfedgeMark(he->getMutable(), glm::vec4(1, 0, 1, 1), glm::vec4(1, 0, 1, 1));
+	//	}
+	//}
+
+}
+
+void MorseFunction::paintWaveFunction() {
+	for (auto& idHe : mesh->getHalfedges()) {
+		const Halfedge* he = &idHe.second;
+		double v = getScalarFunction(he->getTarget()) - getScalarFunction(he->getSource());
+
+		if (v > 0) {
+			display->setHalfedgeMark(he->getMutable(), glm::vec4(0, 0, 0, 1), glm::vec4(1, 1, 1, 1));
+		}
+		if (v < 0) {
+			display->setHalfedgeMark(he->getMutable(), glm::vec4(1, 1, 1, 1), glm::vec4(0, 0, 0, 1));
+		}
+		if (v == 0) {
+			display->setHalfedgeMark(he->getMutable(), glm::vec4(0, 0, 0, 1), glm::vec4(0, 0, 0, 1));
+		}
+	}
 }
--- src/msc/msc.h
+++ src/msc/msc.h
@@ -7,14 +7,16 @@ namespace quadro {
 			NONE, REGULAR, MAXIMUM, MINIMUM, SADDLE
 		};
 		struct Traceline {
-			const Vertex* end1;
-			const Vertex* end2;
+			const Vertex* end1; // saddle point
+			const Vertex* end2; // extreme point
 			std::deque<const Halfedge*> line;
+			bool toMaximum;
+			bool isValid = true;
 		};
 		std::unique_ptr<MeshAttribute<double>> scalarFunction;
 		std::unique_ptr<MeshAttribute<VertexType>> vertexType;
 		std::vector<const Vertex*> saddleVertices;
-		std::vector<Traceline> tracelines;
+		std::vector<std::unique_ptr<Traceline>> tracelines;
 		std::unique_ptr<MeshAttribute<std::vector<Traceline*>>> vertexTracelines;
 	public:
 		MorseFunction(Mesh* mesh);
@@ -23,11 +25,12 @@ namespace quadro {
 		void extractMorseFunction();
 		void extractWaveFunction();
 		void cleanUpMorseFunction();
+		void extractQuasiDualComplex();
 		void paintMorseFunction();
 		void markMorseFunctionPoints();
 		void paintWaveFunction();
 		void smoothScalarFunction(const Vertex* v);
-
+		void removeMorseFunctionVertex(const Vertex* v);
 		void operator()();
 		double getScalarFunction(const Vertex* v);
 		double getGradientScalarFunction(const Halfedge* he);
@@ -36,5 +39,6 @@ namespace quadro {
 		Traceline* traceToMaximum(const Halfedge* he);
 		Traceline* traceToMinimum(const Halfedge* he);
 		Traceline* trace(const Halfedge* he, bool toMaximum);
+		Traceline* trace(const Vertex* end1, const Vertex* end2);
 	};
 }
